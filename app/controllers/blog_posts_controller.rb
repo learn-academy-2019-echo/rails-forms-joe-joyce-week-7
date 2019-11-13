fc62397a0f7cd0b0ac74ebd214be03951c2cea05
Ubuntu <ubuntu@ip-172-31-38-142.us-east-2.compute.internal>
2019-11-13 00:15:21 +0000

    got it to work yay

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,18 @@ class BlogPostsController < ApplicationController
     @post = BlogPost.find(params[:id])
     #render "show.html.erb"
   end
+  
+  def new
+  end 
+  
+  def create
+    @post = BlogPost.create(title: params[:title],
+    content: params[:content])
+    if @post.valid?
+      redirect_to @post
+    else
+      render action: :new
+    end
+  end
+  
 end
